9a6b0c03c3430944f5471d37753433fd66664af2
minz <ghdalswl77@naver.com>
2022-10-07 21:40:16 +0900

    [ADD] #7 Order Controller 작성

---
+++ src/main/java/com/minji/vanillashop/domain/order/OrderController.java
@@ -0,0 +1,55 @@
+package com.minji.vanillashop.domain.order;
+import com.minji.vanillashop.domain.order.entity.Order;
+import com.minji.vanillashop.domain.member.entity.Member;
+import com.minji.vanillashop.domain.member.service.MemberService;
+import com.minji.vanillashop.domain.order.entity.OrderSearch;
+import com.minji.vanillashop.domain.order.service.OrderService;
+import com.minji.vanillashop.domain.product.entity.Product;
+import com.minji.vanillashop.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ProductService productService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Product> products = productService.findProducts();
+
+        model.addAttribute("members", members);
+        model.addAttribute("product", products);
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("productId") Long productId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, productId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+}
