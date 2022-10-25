9a6d17e0fea8b7d31cff3da2d3ae68a6c03a1e5c
minz <ghdalswl77@naver.com>
2022-10-25 23:22:37 +0900

    [ADD] #24 주문 조회 메서드, 조회 dto 추가

--- src/main/java/com/minji/vanillashop/domain/api/OrderApiController.java
+++ src/main/java/com/minji/vanillashop/domain/api/OrderApiController.java
@@ -1,13 +1,24 @@
 package com.minji.vanillashop.domain.api;
 
+import com.minji.vanillashop.domain.order.entity.Order;
+import com.minji.vanillashop.domain.order.entity.OrderItem;
+import com.minji.vanillashop.domain.order.entity.OrderStatus;
 import com.minji.vanillashop.domain.order.repository.OrderRepository;
 import com.minji.vanillashop.domain.order.service.OrderService;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 
 @RestController
@@ -25,5 +36,50 @@ public class OrderApiController {
         return new ResponseEntity<>(orderService.createOrder(memberId, productId, count), HttpStatus.OK);
     }
 
+    @GetMapping("/api/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private List<OrderItemDto> orderItems;
+
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getProduct().getTitle();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 
 }
