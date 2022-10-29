af5907a57eb6eea38b0b56b0cc254c510b8cfd4d
minz <ghdalswl77@naver.com>
2022-10-29 21:19:15 +0900

    [REF] #32 OrderRepository -> OrderQuerydslRepository 변경

--- src/main/java/com/minji/vanillashop/domain/order/repository/OrderQuerydslRepository.java
+++ src/main/java/com/minji/vanillashop/domain/order/repository/OrderQuerydslRepository.java
@@ -1,5 +1,11 @@
 package com.minji.vanillashop.domain.order.repository;
 
+import com.minji.vanillashop.domain.order.dto.domain.MemberOrderDetail;
+import com.minji.vanillashop.domain.order.dto.request.MemberOrderQuery;
+
+import java.util.List;
+
 public interface OrderQuerydslRepository {
 
+    List<MemberOrderDetail> findMemberOrderDetailListBy(MemberOrderQuery query);
 }
--- src/main/java/com/minji/vanillashop/domain/order/repository/OrderRepository.java
+++ src/main/java/com/minji/vanillashop/domain/order/repository/OrderRepository.java
@@ -1,102 +1,90 @@
 package com.minji.vanillashop.domain.order.repository;
 
 import com.minji.vanillashop.domain.order.entity.Order;
-import com.minji.vanillashop.domain.order.entity.OrderSearch;
-import com.minji.vanillashop.domain.order.entity.OrderStatus;
-import com.minji.vanillashop.domain.order.entity.QOrder;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-@Repository
-@RequiredArgsConstructor
-public class OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, String>, OrderQuerydslRepository {
 
-
-    private final EntityManager em;
-
-    public void save(Order order) {
-        em.persist(order);
-    }
-
-    public Order findById(Long orderId) {
-        return em.find(Order.class, orderId);
-    }
-
-    public List<Order> findAllByString(OrderSearch orderSearch) {
-
-        String jpql = "select o from Order o join o.member m";
-        boolean isFirstCondition = true;
-
-        //주문 상태 검색
-        if (orderSearch.getOrderStatus() != null) {
-            if (isFirstCondition) {
-                jpql += " where";
-                isFirstCondition = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " o.status = :status";
-        }
-
-        //회원 이름 검색
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            if (isFirstCondition) {
-                jpql += " where";
-                isFirstCondition = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " m.name like :name";
-        }
-
-        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-                .setMaxResults(1000);
-
-        if (orderSearch.getOrderStatus() != null) {
-            query = query.setParameter("status", orderSearch.getOrderStatus());
-        }
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            query = query.setParameter("name", orderSearch.getMemberName());
-        }
-
-        return query.getResultList();
-    }
-
-    private BooleanExpression statusEq(OrderStatus statusCond){
-        if (statusCond == null){
-            return null;
-        }
-        return QOrder.order.status.eq(statusCond);
-    }
-
-    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
-        return em.createQuery(
-                "select o from Order o" +
-                        " join fetch o.member m" +
-                        " join fetch o.delivery d", Order.class)
-                .setFirstResult(offset)
-                .setMaxResults(limit)
-                .getResultList();
-    }
-
-    public List<Order> findAllWithProduct() {
-        return em.createQuery(
-                "select distinct o from Order o" +
-                        " join fetch o.member m" +
-                        " join fetch o.delivery d" +
-                        " join fetch o.orderItems oi" +
-                        " join fetch oi.product p", Order.class)
-                .setFirstResult(1)
-                .setMaxResults(10)
-                .getResultList();
-    }
+//
+//    private final EntityManager em;
+//
+//    public void save(Order order) {
+//        em.persist(order);
+//    }
+//
+//    public Order findById(Long orderId) {
+//        return em.find(Order.class, orderId);
+//    }
+//
+//    public List<Order> findAllByString(OrderSearch orderSearch) {
+//
+//        String jpql = "select o from Order o join o.member m";
+//        boolean isFirstCondition = true;
+//
+//        //주문 상태 검색
+//        if (orderSearch.getOrderStatus() != null) {
+//            if (isFirstCondition) {
+//                jpql += " where";
+//                isFirstCondition = false;
+//            } else {
+//                jpql += " and";
+//            }
+//            jpql += " o.status = :status";
+//        }
+//
+//        //회원 이름 검색
+//        if (StringUtils.hasText(orderSearch.getMemberName())) {
+//            if (isFirstCondition) {
+//                jpql += " where";
+//                isFirstCondition = false;
+//            } else {
+//                jpql += " and";
+//            }
+//            jpql += " m.name like :name";
+//        }
+//
+//        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+//                .setMaxResults(1000);
+//
+//        if (orderSearch.getOrderStatus() != null) {
+//            query = query.setParameter("status", orderSearch.getOrderStatus());
+//        }
+//        if (StringUtils.hasText(orderSearch.getMemberName())) {
+//            query = query.setParameter("name", orderSearch.getMemberName());
+//        }
+//
+//        return query.getResultList();
+//    }
+//
+//    private BooleanExpression statusEq(OrderStatus statusCond){
+//        if (statusCond == null){
+//            return null;
+//        }
+//        return QOrder.order.status.eq(statusCond);
+//    }
+//
+//    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+//        return em.createQuery(
+//                "select o from Order o" +
+//                        " join fetch o.member m" +
+//                        " join fetch o.delivery d", Order.class)
+//                .setFirstResult(offset)
+//                .setMaxResults(limit)
+//                .getResultList();
+//    }
+//
+//    public List<Order> findAllWithProduct() {
+//        return em.createQuery(
+//                "select distinct o from Order o" +
+//                        " join fetch o.member m" +
+//                        " join fetch o.delivery d" +
+//                        " join fetch o.orderItems oi" +
+//                        " join fetch oi.product p", Order.class)
+//                .setFirstResult(1)
+//                .setMaxResults(10)
+//                .getResultList();
+//    }
 
 
 //    public List<OrderQueryDto> findOrderDtos() {
