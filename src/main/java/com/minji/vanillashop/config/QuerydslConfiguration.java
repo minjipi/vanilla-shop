93d0e8d9348807498defebeccf7b9b82a93297c7
minz <ghdalswl77@naver.com>
2022-10-29 22:19:38 +0900

    [ADD] #33 QuerydslConfiguration 생성

---
+++ src/main/java/com/minji/vanillashop/config/QuerydslConfiguration.java
@@ -0,0 +1,28 @@
+package com.minji.vanillashop.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfiguration {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
+
+//@Configuration
+//public class QuerydslConfiguration {
+//    @Bean
+//    public JPAQueryFactory jpaQueryFactory(EntityManager em){
+//        return new JPAQueryFactory(em);
+//    }
+//}
