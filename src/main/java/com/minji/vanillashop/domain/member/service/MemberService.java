91be84f13f44b0d97d9f3e87bcc49cf296d2b152
minz <ghdalswl77@naver.com>
2022-11-02 09:31:33 +0900

    [ADD] #40 MemberService - Email로 Member 검색 로직 추가

--- src/main/java/com/minji/vanillashop/domain/member/service/MemberService.java
+++ src/main/java/com/minji/vanillashop/domain/member/service/MemberService.java
@@ -1,7 +1,12 @@
 package com.minji.vanillashop.domain.member.service;
 
 
+import com.minji.vanillashop.domain.member.dto.domain.MemberDetailInfo;
+import com.minji.vanillashop.domain.member.dto.request.MemberListQuery;
+import com.minji.vanillashop.domain.member.dto.request.PostMemberDto;
+import com.minji.vanillashop.domain.member.dto.request.UpdateMemberDto;
 import com.minji.vanillashop.domain.member.entity.Member;
+import com.minji.vanillashop.domain.member.repository.MemberQuerydslRepository;
 import com.minji.vanillashop.domain.member.repository.MemberRepository;
 import com.minji.vanillashop.global.exceptions.MemberEmailDuplicateException;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final MemberQuerydslRepository memberQuerydslRepository;
 
     @Transactional
     public Long joinMember(Member member) {
@@ -24,29 +29,58 @@ public class MemberService {
         return member.getId();
     }
 
+    @Transactional
+    public Member apijoinMember(PostMemberDto postMemberDto) {
+        validMemberEmailDuplicate(postMemberDto.getEmail());
+        postMemberDto.validation();
+        return memberRepository.save(postMemberDto.toEntity());
+    }
+
     //회원 아이디 중복검증
     private void validMemberEmailDuplicate(String email) {
         if (memberRepository.existsByEmail(email))
             throw new MemberEmailDuplicateException("이미 사용중인 이메일 입니다.");
     }
 
-    public List<Member> findMembers() {
+
+    public List<Member> readMemberList() {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findOne(String memberId) {
-        return memberRepository.findById(memberId);
+    @Transactional(readOnly = true)
+    public List<MemberDetailInfo> readMemberListByEmail(MemberListQuery query){
+        return memberQuerydslRepository.findOrderDetailByMemberBy(query);
     }
+
+
+//    @Transactional(readOnly = true)
+//    public List<MemberOrderDetail> readMemberOrderDetailList(String memberEmail){
+//        return memberRepository.findMemberOrderDetailListBy(memberEmail);
+//    }
+
+
+//    public Optional<Member> findOne(String memberId) {
+//        return memberRepository.findById(memberId);
+//    }
 
     /**
      * 회원 수정
+     * @return
      */
+//    @Transactional
+//    public void update(String id, String name) {
+//        Member member = memberRepository.findById(id).orElseThrow(
+//                () -> new NoSuchElementException());
+//        member.updateMember(name);
+//    }
+
     @Transactional
-    public void update(String id, String name) {
+    public Member update(Long id, UpdateMemberDto updateMemberDto) {
         Member member = memberRepository.findById(id).orElseThrow(
-                ()-> new NoSuchElementException());
+                () -> new NoSuchElementException());
+        member.updateMember(updateMemberDto.getName());
 
-        member.updateMember(name);
-
+        return member;
     }
+
 }
